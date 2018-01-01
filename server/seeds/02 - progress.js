f68edd2fcef9cbea36dde35e6a22874082fce9e7
llamacoder <30612433+llamacoder@users.noreply.github.com>
2018-01-01 09:16:50 -0700

    resetting seeds

--- server/seeds/02 - progress.js
+++ server/seeds/02 - progress.js
@@ -5,15 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('progress').insert([
-        {id: 1, user_id: 1, date: new Date("1/1/2018"), progress: 100},
-        {id: 2, user_id: 1, date: new Date("1/2/2018"), progress: 100},
-        {id: 3, user_id: 1, date: new Date("1/3/2018"), progress: 100},
-        {id: 4, user_id: 2, date: new Date("1/1/2018"), progress: 100},
-        {id: 5, user_id: 2, date: new Date("1/2/2018"), progress: 100},
-        {id: 6, user_id: 2, date: new Date("1/3/2018"), progress: 100},
-        {id: 7, user_id: 2, date: new Date("1/4/2018"), progress: 100},
-        {id: 8, user_id: 3, date: new Date("1/1/2018"), progress: 100},
-        {id: 9, user_id: 3, date: new Date("1/3/2018"), progress: 200},
+        {id: 1, user_id: 1, date: new Date("1/1/2018"), progress: 0},
+        {id: 2, user_id: 2, date: new Date("1/1/2018"), progress: 0},
+        {id: 3, user_id: 3, date: new Date("1/1/2018"), progress: 0}
       ]);
     })
   .then(function() {
